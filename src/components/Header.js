50c999d68909fb59daf9ee4161585815049d6e77
Mohit18410 <mohitgoyal052002@gmail.com>
2024-06-04 21:53:16 +0530

    Installing Axios and Removing Redundance in Header Tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,13 @@ import "./header.css";
 export const Header = () => {
   return (
     <div className="header-container py-1 lg:px-12 md:px-7 sm:px-6 px-5">
-      <div className="flex justify-between items-center">
+      <div className="flex-vertical justify-between">
         <div className=" w-20">
           <Link>
             <img src={imgPath} alt="Not Found" className=" w-full" />
           </Link>
         </div>
-        <div className="flex justify-between items-center text-center w-auto gap-1 sm:gap-4 2xl:text-xl xl:text-lg md:text-md">
+        <div className="flex-vertical justify-between text-center w-auto gap-1 sm:gap-4 2xl:text-xl xl:text-lg md:text-md">
           <NavLink to="/" className="links">
             Home
           </NavLink>
@@ -22,7 +22,7 @@ export const Header = () => {
           <NavLink to="contact" className="links">
             Contact Us
           </NavLink>
-          <div className=" w-6 flex justify-center">
+          <div className=" w-6 flex-horizontal">
             <FaRegUserCircle
               size={"1.5em"}
               className="sign-icon fill-white hover:cursor-pointer"
