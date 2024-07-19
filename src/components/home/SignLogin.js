f624558972ce58c20605cd216022179b73ba454b
Mohit18410 <mohitgoyal052002@gmail.com>
2024-07-19 10:50:41 +0530

    Adding Navigation to Sign/LogIn in Home Page

--- src/components/home/SignLogin.js
+++ src/components/home/SignLogin.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const buttonProperty =
   "sm:text-2xl text-xl py-2 md:px-14 sm:px-10 px-7 rounded-xl border-slate-950 border-2 hover:md:scale-125 hover:scale-110 transition-transform";
 
 const SignLogin = () => {
+  const navigate = useNavigate();
+
+  const handleSignUp = () => {
+    navigate("/signUp");
+  };
+
+  const handleLogIn = () => {
+    navigate("/logIn");
+  };
+
   return (
     <div className=" my-7 mx-7 flex-horizontal-vertical md:gap-12 sm:gap-10 gap-8">
       <button
@@ -12,6 +23,7 @@ const SignLogin = () => {
           color: "rgb(253, 240, 213)",
         }}
         className={buttonProperty}
+        onClick={handleSignUp}
       >
         SignUp
       </button>
@@ -21,6 +33,7 @@ const SignLogin = () => {
           color: "rgb(253, 240, 213)",
         }}
         className={buttonProperty}
+        onClick={handleLogIn}
       >
         LogIn
       </button>
